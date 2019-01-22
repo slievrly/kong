204cc56e9dba72edeb0f322b15aa8bb3bdaf4b5d
Enrique García Cota <kikito@gmail.com>
2019-01-22 17:51:05 +0100

    feat(plugins) add new protocols field to plugins

--- kong/db/migrations/core/003_100_to_110.lua
+++ kong/db/migrations/core/003_100_to_110.lua
@@ -7,14 +7,26 @@ return {
       UPDATE upstreams SET created_at = DATE_TRUNC('seconds', created_at);
       UPDATE targets   SET created_at = DATE_TRUNC('milliseconds', created_at);
 
-
       DROP FUNCTION IF EXISTS "upsert_ttl" (TEXT, UUID, TEXT, TEXT, TIMESTAMP WITHOUT TIME ZONE);
 
+      DO $$
+      BEGIN
+        ALTER TABLE IF EXISTS ONLY "plugins" ADD "protocols" TEXT[];
+      EXCEPTION WHEN DUPLICATE_COLUMN THEN
+        -- Do nothing, accept existing state
+      END;
+      $$;
+
+      CREATE INDEX IF NOT EXISTS "plugins_protocols_idx" ON "plugins" ("protocols");
     ]],
   },
 
   cassandra = {
     up = [[
+
+      ALTER TABLE plugins ADD protocols set<text>;
+      CREATE INDEX IF NOT EXISTS ON plugins(protocols);
+
     ]],
   },
 }
